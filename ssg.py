2835dda480f547852d70d21aa9dfca5ea3656f1f
Julie Chao <julie_chao@yahoo.com.au>
2020-11-08 13:21:28 +0800

    completed module

---
+++ ssg.py
@@ -0,0 +1,10 @@
+
+from ssg.site import Site
+
+def main(source="content", dest = "dist"):
+    config = {"source": source, "dest": dest}
+
+    Site(**config).build()
+
+if __name__ == "__main__":
+    main()
--- ssg/site.py
+++ ssg/site.py
@@ -1,35 +1,18 @@
-import Path from pathlib
+from pathlib import Path
 
+class Site:
 
-class Site(self, source="content", dest="dist"):
     def __init__(self, source, dest):
-
-        self._source = Path(source)
-        self._dest = Path(dest)
-
-    def source(self):
-        return self._source
-
-    def dest(self):
-        return self._dest
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest()+ "/" + relative_to(self.source())
-        mkdir(directory, parents = True, exist_ok = True)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+        print(f"making directory in dest {directory}")
 
     def build(self):
-        mkdir(self.dest(), parents=True, exist_ok = True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
-            if __name__ == "__main__":
-                if path.isdir():
-                    create_dir(path)
-
-    def main():
-        config = { "source": source, "dest": dest}
-        mysite = Site(**config)
-
-    if __name__ == "__main__":
-        main()
-
-
-    
+            if path.is_dir():
+                self.create_dir(path)
